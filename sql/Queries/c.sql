ea32c4eff0abceca2e5bfd10f95d15a1243322b5
Kris <kristhecanadian101@gmail.com>
2022-02-28 16:20:12 -0500

    question 3c

---
+++ sql/Queries/c.sql
@@ -0,0 +1,9 @@
+SELECT genre
+FROM (
+    SELECT genre, COUNT(movies.title) as count
+    FROM movies, genres
+    WHERE movies.mid = genres.mid
+    GROUP BY genres.genre
+    ORDER BY count DESC
+) as gc
+WHERE gc.count > 1000

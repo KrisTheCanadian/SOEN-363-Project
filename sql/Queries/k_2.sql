785125ce54e172a1ad6a90fa0345d62781559b67
Vincent Bruzzese <vincentbruzzese.99@gmail.com>
2022-03-05 00:24:55 -0500

    completed question k

--- sql/Queries/k_2.sql
+++ sql/Queries/k_2.sql
@@ -1,5 +1,12 @@
-SELECT a.name, m.mid
-FROM actors a, movies m
-WHERE a.name in (SELECT DISTINCT a.name
-FROM actors a)
-    and a.mid = m.mid
+CREATE VIEW most_popular AS
+SELECT a1.name, count( DISTINCT a2.name) as coactors
+FROM actors a1, actors a2
+WHERE a1.mid = a2.mid
+AND a1.name <> a2.name
+GROUP BY a1.name
+ORDER BY coactors DESC;
+
+SELECT mp.name, mp.coactors
+FROM most_popular mp
+WHERE  mp.coactors >= (SELECT max(mp.coactors)
+        FROM most_popular mp)
